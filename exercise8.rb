64eda59852edb68832351aa0c2b04418dc1d745c
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-09-19 17:44:29 -0400

    Simpler method for asking for running details

--- exercise8.rb
+++ exercise8.rb
@@ -3,33 +3,45 @@ def speed(distance_in_metres, time_in_minutes)
   distance_in_metres.to_f/(time_in_minutes.to_f * 60)
 end
 
-persons = []
-counter = 1
+# persons = []
+# counter = 1
+#
+# 3.times do
+#   puts "How far did this person run (in metres)?"
+#   user_distance = gets
+#   puts "How long (in minutes) did it take this person take to run take to run #{user_distance} metres??"
+#   user_time = gets
+#   person = ["person #{counter}", speed(user_distance, user_time)]
+#   persons << person
+#   counter += 1
+# end
+#
+# p persons
+#
+# if persons[0][1] > persons[1][1] && persons[0][1] > persons[2][1]
+#   puts "Person 1 is the fastest"
+# elsif persons[1][1] > persons[2][1] && persons[1][1] > persons[0][1]
+#   puts "Person 2 is the fastest"
+# elsif persons[2][1] > persons[0][1] && persons[2][1] > persons[1][1]
+#   puts "Person 3 is the fastest"
+# elsif persons[1][1] == persons[2][1] && persons[2][1] == persons[0][1]
+#   puts "Everyone tied with the same speed of #{persons[1][1]}m/s."
+# else
+#   puts "Well done everyone!"
+# end
 
-3.times do
-  puts "How far did this person run (in metres)?"
-  user_distance = gets
-  puts "How long (in minutes) did it take this person take to run take to run #{user_distance} metres??"
-  user_time = gets
-  person = ["person #{counter}", speed(user_distance, user_time)]
-  persons << person
-  counter += 1
+def ask_for_running_details
+  puts "How far did person run (in metres)?"
+  distance = gets.to_f
+  puts "How long (in minutes) did person run take to run #{distance} metres?"
+  mins = gets.to_f
+  puts "They went at a speed of #{distance/(mins*60)}m/s."
+  speed = distance/(mins*60)
 end
 
-p persons
-
-if persons[0][1] > persons[1][1] && persons[0][1] > persons[2][1]
-  puts "Person 1 is the fastest"
-elsif persons[1][1] > persons[2][1] && persons[1][1] > persons[0][1]
-  puts "Person 2 is the fastest"
-elsif persons[2][1] > persons[0][1] && persons[2][1] > persons[1][1]
-  puts "Person 3 is the fastest"
-elsif persons[1][1] == persons[2][1] && persons[2][1] == persons[0][1]
-  puts "Everyone tied with the same speed of #{persons[1][1]}m/s."
-else
-  puts "Well done everyone!"
-end
-
+speed1 = ask_for_running_details
+speed2 = ask_for_running_details
+speed3 = ask_for_running_details
 
 #
 # puts "How far did person 2 run (in metres)?"
@@ -46,14 +58,14 @@ end
 # speed2 = speed(distance2/mins2)
 # speed3 = speed(distance3/mins3)
 #
-# if speed3 > speed2 && speed3 > speed1
-#   puts "Person 3 was the fastest at #{speed3} m/s"
-# elsif speed2 > speed3 && speed2 > speed1
-#   puts "Person 2 was the fastest at #{speed2} m/s"
-# elsif speed1 > speed3 && speed1 > speed2
-#   puts "Person 1 was the fastest at #{speed1} m/s"
-# elsif speed1 == speed2 && speed2 == speed3
-#   puts "Everyone tied at #{speed1} m/s"
-# else
-#   puts "Well done everyone!"
-# end
+if speed3 > speed2 && speed3 > speed1
+  puts "Person 3 was the fastest at #{speed3} m/s"
+elsif speed2 > speed3 && speed2 > speed1
+  puts "Person 2 was the fastest at #{speed2} m/s"
+elsif speed1 > speed3 && speed1 > speed2
+  puts "Person 1 was the fastest at #{speed1} m/s"
+elsif speed1 == speed2 && speed2 == speed3
+  puts "Everyone tied at #{speed1} m/s"
+else
+  puts "Well done everyone!"
+end
