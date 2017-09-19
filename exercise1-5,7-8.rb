f0aabf50472bb5970a742ce781420d9cb5933f2a
JOSH DALES <DALES.JOSH@GMAILCOM>
2017-09-19 16:03:48 -0400

    Rename exercise1-5,7.rb

--- exercise1-5,7-8.rb
+++ exercise1-5,7-8.rb
@@ -52,7 +52,7 @@ end
 #Exercise 8
 
 def speed(distance_in_metres, time_in_minutes)
-  distance.to_f/(time.to_f * 60)
+  distance.to_f/(time.to_f / 60)
 end
 
 puts "How far did person 1 run (in metres)?"
@@ -60,24 +60,19 @@ distance1 = gets.to_f
 puts "How long (in minutes) did person 1 run take to run #{distance1} metres?"
 mins1 = gets.to_f
 
-
 puts "How far did person 2 run (in metres)?"
 distance2 = gets.to_f
 puts "How long (in minutes) did person 2 take to run #{distance2} metres?"
 mins2 = gets.to_f
-
 
 puts "How far did person 3 run (in metres)?"
 distance3 = gets.to_f
 puts "How long (in minutes) did person 3 take to run #{distance3} metres?"
 mins3 = gets.to_f
 
-secs1 = mins1 * 60
-speed1 = distance1/secs1
-secs2 = mins2 * 60
-speed2 = distance2/secs2
-secs3 = mins3 * 60
-speed3 = distance3/secs3
+speed1 = speed(distance1/mins1)
+speed2 = speed(distance2/mins2)
+speed3 = speed(distance3/mins3)
 
 if speed3 > speed2 && speed3 > speed1
   puts "Person 3 was the fastest at #{speed3} m/s"
